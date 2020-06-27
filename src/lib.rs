6f6a1f03fd727263988afa783a2185127abb8fc5
Lucas Radaelli <lucasradaelli@google.com>
2020-06-27 05:12:37 +0000

    Adds unit test files

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+#[cfg(test)]
+mod tests;
+
 pub fn hello_world() {
     println!("Hello, world!");
 }
---
+++ src/tests/mod.rs
@@ -0,0 +1,6 @@
+//#[cfg(test)]
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
