d4c48c0c37a32e539afd3b34cfbb983b253be35d
Lucas Radaelli <lucasradaelli@google.com>
2020-06-28 04:31:12 +0000

    Basic definition of the PositionConverter

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,69 @@
 #[cfg(test)]
 mod tests;
 
+use std;
+use std::str;
+use pgn_reader::{SanPlus, Skip, Visitor, RawHeader, RawComment,  Nag, Outcome};
+
+struct PositionConverter {
+    starting_fen: String,
+    final_description: String,
+}
+
+impl PositionConverter {
+    fn new() -> PositionConverter {
+        PositionConverter {
+            starting_fen: String::from(""),
+            final_description: String::from(""),
+        }
+    }
+}
+
+impl Visitor for PositionConverter {
+    type Result = String;
+    fn begin_game(&mut self) {}
+
+    fn begin_headers(&mut self) {}
+
+    fn header(&mut self, _key: &[u8], _value: RawHeader) {
+let key_str = match str::from_utf8(_key) {
+    Ok(v)  => v,
+    Err(e) => panic!("Invalid UTF-8 sequence in fen header key: {}", e),
+};
+if (key_str == "FEN" && self.starting_fen.is_empty()) {
+    let fen = _value.decode_utf8().unwrap();
+    self.starting_fen.push_str(&fen);
+}
+     }
+
+        fn end_headers(&mut self) -> Skip {
+            Skip(false)
+        }
+
+    fn san(&mut self, _san_plus: SanPlus) {}
+
+    fn nag(&mut self, _nag: Nag) {}
+
+        fn comment(&mut self, _comment: RawComment) {}
+
+    fn begin_variation(&mut self) -> Skip {
+        Skip(true) // stay in the mainline
+    }
+
+    fn end_variation(&mut self) {}
+
+    fn outcome(&mut self, _outcome: Option<Outcome>) {}
+
+    fn end_game(&mut self) -> Self::Result {
+        // TODO: is there a way to return self.final_description directly from this mutable reference?
+        // alternative 1:
+        //self.final_description.clone()
+        // Alternative 2:
+        self.final_description = self.starting_fen.clone();
+        std::mem::replace(&mut self.final_description, String::new())
+    }
+}
+
 pub fn hello_world() {
     println!("Hello, world!");
 }
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,6 +1,33 @@
-//#[cfg(test)]
+use super::*;
+use std::io;
+use pgn_reader::BufferedReader;
 
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    #[test] 
+    fn empty_description()  -> io::Result<()>  {
+        let pgn = b"
+[White \"player1\"]
+[Black \"player2\"]
+";
+        let mut reader = BufferedReader::new_cursor(&pgn[..]);
+        let mut position_converter = PositionConverter::new();
+        let description = reader.read_game(&mut position_converter)?;
+    
+        assert_eq!(description, Some("".to_string()));
+        Ok(())
+    }
+
+    #[test] 
+    fn reads_fen()  -> io::Result<()>  {
+        let pgn = b"
+[White \"player1\"]
+[Black \"player2\"]
+[FEN \"rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2\"]
+        ";
+        let mut reader = BufferedReader::new_cursor(&pgn[..]);
+        let mut position_converter = PositionConverter::new();
+        let description = reader.read_game(&mut position_converter)?;
+    
+        let result = "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2";
+        assert_eq!(description, Some(result.to_string()));
+        Ok(())
     }
